2b5f33e8c06a417ae5a153215a758e62779ab107
nawabsahab16 <Nawab162004@gmail.com>
2024-07-25 12:40:05 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+
+import { Inter as FontSans } from "next/font/google"
+import { cn } from "@/lib/utils";
+
+
+import "./globals.css";
+import { Metadata } from "next";
+
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+ 
+export const metadata : Metadata = {
+  title : "DocSync",
+  description: "Best collaborative editor out there",
+}
+
+export default function RootLayout({ children} : {children: React.ReactNode} ) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
+
