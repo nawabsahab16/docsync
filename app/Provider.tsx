2791291ed1174765d68870bb672be4c0d256f13b
nawabsahab16 <Nawab162004@gmail.com>
2024-08-04 16:37:36 +0530

    feat: real time live sticky comments

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 import {ClientSideSuspense, LiveblocksProvider} from "@liveblocks/react/suspense";
 import { ReactNode } from "react";
 
 const Provider = ({children}: {children: ReactNode}) => {
+
+  const { user: clerkUser } = useUser();
+
   return (
     <LiveblocksProvider authEndpoint="/api/liveblocks-auth"
       resolveUsers = {async ({userIds}) => {
@@ -13,7 +17,19 @@ const Provider = ({children}: {children: ReactNode}) => {
 
         return users;
       }}
-    >
+
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        })
+
+        return roomUsers;
+      }}
+
+
+    > 
       <ClientSideSuspense fallback={<Loader />}>
         {children}
       </ClientSideSuspense>
